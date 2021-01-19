a09e6aadbb5633372ccdb400a188454da85b35c4
Widzmarc Jean Nesly Phelle <widzmarc@gmail.com>
2021-01-19 17:50:29 -0400

    add test for the equal? method

--- spec/board_spec.rb
+++ spec/board_spec.rb
@@ -9,4 +9,16 @@ describe Board do
       expect(my_board.replace(1, 'x')).to eql(true)
     end
   end
+
+  describe '#equal?' do
+    it 'return true if the elements at those index are equal' do
+      board = Board.new
+      symbol = 'X'
+      board.reset_board
+      board.replace(1, symbol)
+      board.replace(4, symbol)
+      board.replace(7, symbol)
+      expect(board.equal?(0, 3, 6)).to eql(true)
+    end
+  end
 end
